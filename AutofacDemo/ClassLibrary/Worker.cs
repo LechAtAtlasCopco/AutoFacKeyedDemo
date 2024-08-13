b42050ed46437f4f925079a0e3aefb6adeb33b3f
Lech Wiecierzynski <lech.wiecierzynski@external.atlascopco.com>
2024-08-13 08:33:37 +0200

    Demo

---
+++ AutofacDemo/ClassLibrary/Worker.cs
@@ -0,0 +1,27 @@
+﻿using Autofac.Features.AttributeFilters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClassLibrary
+{
+    public class Worker
+    {
+        // public Worker([KeyFilter("Source")] List<string> source, [KeyFilter("Target")] List<string> target)
+        public Worker([FromKeyedServices("Source")] List<string> source, [FromKeyedServices("Target")] List<string> target)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(target);
+
+            Console.WriteLine("Source List");
+            foreach (var name in source)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Target List");
+            foreach (var name in target)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ AutofacDemo/ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+using Autofac;
+using Autofac.Features.AttributeFilters;
+using ClassLibrary;
+
+namespace ConsoleApp;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+
+        var builder = new ContainerBuilder();
+
+        var names = new List<string>
+            { "Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Ivan", "Judy" };
+
+        var empty = new List<string>();
+
+
+        builder.RegisterInstance(names)
+            .Keyed<List<string>>("Source")
+            .SingleInstance();
+
+        builder.RegisterInstance(empty)
+            .Keyed<List<string>>("Target")
+            .SingleInstance();
+
+        builder.RegisterType<Worker>().WithAttributeFiltering();
+
+
+        var container = builder.Build();
+        var list1 = container.ResolveKeyed<List<string>>("Source");
+        var list2 = container.ResolveKeyed<List<string>>("Target");
+        var worker = container.Resolve<Worker>();
+    }
+}
