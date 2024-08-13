f7a6a8b568bd4c1097537fe8dd2ade2f5dcefc43
Lech Wiecierzynski <lech.wiecierzynski@external.atlascopco.com>
2024-08-13 13:21:56 +0200

    fix

--- AutofacDemo/ConsoleApp/Program.cs
+++ AutofacDemo/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System;
+using System.Threading.Channels;
 using Autofac;
 using Autofac.Features.AttributeFilters;
 using ClassLibrary;
@@ -28,12 +29,50 @@ public static class Program
             .Keyed<List<string>>("Target")
             .SingleInstance();
 
-        builder.RegisterType<Worker>().WithAttributeFiltering();
-
-
+        builder.RegisterType<Worker>()
+            .WithFromKeyedServices();
+        //.WithAttributeFiltering();
         var container = builder.Build();
         var list1 = container.ResolveKeyed<List<string>>("Source");
         var list2 = container.ResolveKeyed<List<string>>("Target");
         var worker = container.Resolve<Worker>();
+
+
+        // builder.RegisterType<Worker>().WithParameter(
+        //     (parameterInfo, componentContext) =>
+        //     {
+        //         var filter = parameterInfo.GetCustomAttributes<FromKeyedServicesAttribute>(true).FirstOrDefault();
+        //         if (filter == null)
+        //         {
+        //             throw new ArgumentNullException(nameof(filter), "Filter cannot be null");
+        //         }
+        //
+        //         var isRegistered = componentContext.ComponentRegistry.IsRegistered(new Autofac.Core.KeyedService(filter.Key, parameterInfo.ParameterType));
+        //         return isRegistered;
+        //     },
+        //     (parameterInfo, context) =>
+        //     {
+        //         var filter = parameterInfo.GetCustomAttributes<FromKeyedServicesAttribute>(true).First();
+        //         if (filter.Key is string key && context.TryResolveKeyed(key, parameterInfo.ParameterType, out var value))
+        //         {
+        //             return value;
+        //         }
+        //
+        //         throw new InvalidOperationException($"No service registered with key: {filter.Key}");
+        //     });
+
+        // .WithParameter(
+        //     (p, c) =>
+        //     {
+        //         var filter = p.GetCustomAttributes<ParameterFilterAttribute>(true).FirstOrDefault();
+        //         var result = filter is not null && filter.CanResolveParameter(p, c);
+        //         return result;
+        //     },
+        //     (p, c) =>
+        //     {
+        //         var filter = p.GetCustomAttributes<ParameterFilterAttribute>(true).First();
+        //         var result = filter.ResolveParameter(p, c);
+        //         return result;
+        //     })
     }
 }
---
+++ AutofacDemo/ConsoleApp/RegistrationExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp;
+
+public static class RegistrationExtensions
+{
+    public static IRegistrationBuilder<TLimit, TReflectionActivatorData, TRegistrationStyle>
+        WithFromKeyedServices<TLimit, TReflectionActivatorData, TRegistrationStyle>(
+            this IRegistrationBuilder<TLimit, TReflectionActivatorData, TRegistrationStyle> builder)
+        where TReflectionActivatorData : ReflectionActivatorData
+    {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        return builder.WithParameter(
+            (parameterInfo, componentContext) =>
+            {
+                var filter = CustomAttributeExtensions.GetCustomAttributes<FromKeyedServicesAttribute>((ParameterInfo)parameterInfo, true).FirstOrDefault();
+                if (filter == null)
+                {
+                    throw new ArgumentNullException(nameof(filter), "Filter cannot be null");
+                }
+
+                var isRegistered = componentContext.ComponentRegistry.IsRegistered(new Autofac.Core.KeyedService(filter.Key, parameterInfo.ParameterType));
+                return isRegistered;
+            },
+            (parameterInfo, context) =>
+            {
+                var filter = CustomAttributeExtensions.GetCustomAttributes<FromKeyedServicesAttribute>((ParameterInfo)parameterInfo, true).First();
+                if (filter.Key is string key && context.TryResolveKeyed(key, parameterInfo.ParameterType, out var value))
+                {
+                    return value;
+                }
+
+                throw new InvalidOperationException($"No service registered with key: {filter.Key}");
+            });
+    }
+}
